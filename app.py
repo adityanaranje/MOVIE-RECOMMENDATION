b6d9e53f29858f336154211736c932a81743d528
Aditya Naranje <75174363+adityanaranje@users.noreply.github.com>
2021-09-20 17:50:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def recommend(movie):
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+similarity = pickle.load('similarity.pkl')
 
 st.title('Movie Recommender System')
 
